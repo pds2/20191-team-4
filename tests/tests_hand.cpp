901a51804e7a7e2292767cc80e4770335c02e32e
Ícaro Neto <44352014+Icaro-T@users.noreply.github.com>
2019-06-20 23:56:41 -0300

    Update tests_hand.cpp

--- tests/tests_hand.cpp
+++ tests/tests_hand.cpp
@@ -1,1 +1,21 @@
+#include "doctest.h"
 
+#include "hand.h"
+#include "usual_card.h"
+
+TEST_CASE("01 - Testando o construtor") {
+	Deck deck = Deck();
+	deck.shuffle_deck();
+	Hand playerHand = Hand(&deck);
+	//Deck deck1 = *deck;
+	DOCTEST_CHECK_NOTHROW(playerHand);
+}
+
+
+TEST_CASE("02 - Testando numero inicial de cartas na mao"){
+	Deck deck = Deck();
+	deck.shuffle_deck();
+	Hand testHand = Hand(&deck);
+	int size = testHand.hand_size();
+	DOCTEST_CHECK( size == 3);
+}	
