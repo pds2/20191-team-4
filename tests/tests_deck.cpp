9acb139f0583c2b7f9284cb25cbc9fadd643c89f
Ícaro Neto <44352014+Icaro-T@users.noreply.github.com>
2019-06-20 23:29:33 -0300

    Update tests_deck.cpp

--- tests/tests_deck.cpp
+++ tests/tests_deck.cpp
@@ -1,1 +1,14 @@
+#include "doctest.h"
+#include "deck.h"
+
+TEST_CASE("01 - Testando o construtor") {
+	DOCTEST_CHECK_NOTHROW(Deck());
+}
+
+TEST_CASE("02 - Testando a funcao que retira uma carta no topo do deck"){
+	Deck deck = Deck();
+	Card *testCard = deck.draw_card();
+	Rank cardRank = testCard->get_rank();
+	DOCTEST_CHECK(cardRank == FourOfClubs);
+}
 
